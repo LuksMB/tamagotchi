808ded60941e75882591237de9a08dddfa686c16
Lucas M. Braga <lucasmb.7@gmail.com>
2024-12-10 10:09:52 -0300

    Interface pronta
    interface funciona, botões ainda só printando logs.

---
+++ scripts/main.gd
@@ -0,0 +1,16 @@
+extends Node2D
+
+func _on_pink_button_pressed() -> void:
+	print("Botão rosa funcionando!")
+
+func _on_green_button_pressed() -> void:
+	print("Botão verde funcionando!")
+
+func _on_white_button_pressed() -> void:
+	print("Botão branco funcionando!")
+
+func _on_left_arrow_button_pressed() -> void:
+	print("Seta esquerda funcionando!")
+
+func _on_right_arrow_button_pressed() -> void:
+	print("Seta direita funcionando!")
